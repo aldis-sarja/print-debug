f57f8cf0083e603c3825cba44651c488baa3a1d5
aldis-sarja <48488705+aldis-sarja@users.noreply.github.com>
2019-03-13 13:52:54 +0200

    Delete print-dbg.rkt

--- print-debug/print-dbg.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-(provide p-dbg)
-
-(define-syntax-rule (p-dbg body)
-  (let ([result body])
-    (eprintf "[~a:~a] ~v = ~a~%" (syntax-source #'body) (syntax-line #'body) 'body result)
-    result))
